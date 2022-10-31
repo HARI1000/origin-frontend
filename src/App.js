5a12efae13e56f0830f273787eb2df622a6ced4d
Harishankar <you@example.com>
2022-10-31 21:53:50 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Feedback from './Feedback';
 import Home from './Home';
 import Thx from './Thx';
+export const URL = process.env.REACT_APP_SERVER_URL;
 function App(){
  return(
 <Router>
