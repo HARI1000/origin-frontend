9cf7d0dc504d2bd9da82262da6a425af975b955a
Harishankar <you@example.com>
2022-11-01 12:25:32 +0530

    6thh commti

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import App from './App';
+import { Link } from 'react-router-dom';
+<Link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></Link>
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
